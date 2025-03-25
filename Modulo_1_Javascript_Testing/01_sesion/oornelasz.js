cad31870a06f3a9c5dd7b17aa0e976442e53a6f7
oornelasz <oornelasz@megacable.com.mx>
2025-03-25 11:20:43 -0600

    oornelasz
    Oscar Martin Ornelas Zarate
    oornelasz@megacable.com.mx

---
+++ Modulo_1_Javascript_Testing/01_sesion/oornelasz.js
@@ -0,0 +1,11 @@
+var int = 123456;
+var string = "texto";
+var boolean = true;
+
+var nombre = "Oscar Martin";
+var apellido = 'Ornelas';
+var nombreCompleto = `${nombre} ${apellido}`;  
+var edad = 44;
+
+console.log(`Bienvenido a l sitio de oornelasz ${nombreCompleto} y tengo ${edad} años`);
+console.log("Hola, soy" + nombreCompleto + "y tengo" + edad + "años");
