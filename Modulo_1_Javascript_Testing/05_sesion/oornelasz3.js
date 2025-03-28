ffd7f3d97d1b04d585adb5041616a1b3af520c48
oornelasz <oornelasz@megacable.com.mx>
2025-03-28 10:27:11 -0600

    oornelasz
    oornelasz@megacable.com.mx

---
+++ Modulo_1_Javascript_Testing/05_sesion/oornelasz3.js
@@ -0,0 +1,7 @@
+const titulo = document.getElementById ("Titulo")
+const parrafos = document.getElementsByClassName ("parrafo")
+
+console.log (titulo.textContent);
+
+titulo.textContent = "Bienvenido a mi super pagina WEB"
+parrafos.style.color = "blue"
