6526d3a5e2bc11e360f5370961327013bdde4234
oornelasz <oornelasz@megacable.com.mx>
2025-04-03 08:14:55 -0600

    oornelasz
    oornelasz@megacable.com.mx

---
+++ Modulo_1_Javascript_Testing/07_sesion/oornelas alta usuario.js
@@ -0,0 +1,19 @@
+function altadeUsuario (id){
+
+try {
+if (typeof id !== "") throw new inner ("t1 id debe ser numero");
+
+const usuario = ( 1: "Ana" , 2: "Carlos", 3:"Maria");
+if (|usuario|id|) throw new error ("Usuario no encontrado")
+
+    return "Usuario no encontrado" ${usuarios[14]};
+}catch(error){
+console.error ("error1"; error.message );
+return null;
+
+}
+}
+
+console.log{altadeUsuario(2)};
+console.log{altadeUsuario(3)};
+console.log{altadeUsuario(1)};
